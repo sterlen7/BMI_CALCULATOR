5edcc3fe048d913afde89042d0fbab0dce288ed6
sterlen7 <102663057+sterlen7@users.noreply.github.com>
2023-03-07 11:48:09 +0000

    Update bmi.dart

--- bmi.dart
+++ bmi.dart
@@ -16,4 +16,11 @@ double heightInput() {
 void main() {
   double bmiResult = weightInput() / pow(heightInput(), 2);
   print(' Your BMI is $bmiResult');
+   if (bmiResult < 18) {
+    print(' You are Under weight');
+  } else if (bmiResult <= 29) {
+    print('You have a normal weight');
+  } else if (bmiResult > 30) {
+    print('You are Obese. Hit the gym ');
+  }
 }
